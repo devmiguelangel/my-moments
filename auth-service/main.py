8e9fb6b3d06f93a52739eec240e63d461b760d99
Miguel Angel <me@miguelangel.co>
2023-12-28 23:53:51 -0400

    chore: add ruff rules

--- auth-service/main.py
+++ auth-service/main.py
@@ -3,12 +3,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 
-@app.get("/")
+@app.get('/')
 async def root():
+    """Root path."""
+
     return {
-        "message": "Hello World1",
-        "message1": "Hello World2",
-        "message2": "Hello World3",
-        "message3": "Hello World4",
-        "message4": "Hello World5",
+        'message': 'Hello World1',
+        'message1': 'Hello World2',
+        'message2': 'Hello World3',
+        'message3': 'Hello World4',
+        'message4': 'Hello World5',
     }
